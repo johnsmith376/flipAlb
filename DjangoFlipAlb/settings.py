39dacff52b77dc56fa75f730fcd9b8d46b10983a
johnsmith376 <hschmidt0032@aol.com>
2026-02-02 21:30:49 -0500

    fix host

--- DjangoFlipAlb/settings.py
+++ DjangoFlipAlb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-xjgch#ljg-s5v)&#iuq!)2x%9^q&*9m-#sn75leies$lzw*ufu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','https://flipalb.onrender.com/']
+ALLOWED_HOSTS = ['127.0.0.1','flipalb.onrender.com']
 
 
 # Application definition
